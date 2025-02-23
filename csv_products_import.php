2dd12c2ad8eae181359961980e1eb3b60f86b059
Mftr96 <matteo.federici96@gmail.com>
2025-02-23 22:28:03 +0100

    importing products csv

--- csv_products_import.php
+++ csv_products_import.php
@@ -3,10 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "php_csv_import";
-$csvName = "categorie.csv";
+$csvName = "prodotti.csv";
 
 echo file_exists($csvName) ? "il file $csvName esiste! <br>" : "il file $csvName non è stato trovato, controlla che ci sia nella cartella";
-$dataImport = [];
 $readCsv = fopen($csvName, 'r');
 fgetcsv($readCsv);
 
@@ -21,10 +20,13 @@ try {
         print_r($data[1]); // Stampa l'array con i dati della riga
         $id = $data[0];
         $name = $data[1];
-        $sql = "INSERT IGNORE INTO categories (id, `name`) VALUES (:id, :name)";
+        $price = $data[2];
+        $cat_id = $data[3];
+        //values has placeholders written with ":", in execute put values you want to insert in db
+        $sql = "INSERT IGNORE INTO products (id, `name`, price,category_id) VALUES (:id, :name, :price, :category_id)";
         $stmt = $conn->prepare($sql);
-        $stmt->execute(['id' => $id, 'name' => $name]);
-        echo "New record created successfully";
+        $stmt->execute(['id' => $id, 'name' => $name, 'price'=>$price,'category_id'=>$cat_id]);
+        echo "New record created successfully <br>";
     }
 
     fclose($readCsv); // Chiudi il file dopo aver finito
