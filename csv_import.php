fcbf0c8449758f0e18635c3816de0d8ba8eb835a
Mftr96 <matteo.federici96@gmail.com>
2025-02-23 22:15:48 +0100

    imported categories.csv in db

---
+++ csv_import.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "php_csv_import";
+$csvName = "categorie.csv";
+
+echo file_exists($csvName) ? "il file $csvName esiste! <br>" : "il file $csvName non è stato trovato, controlla che ci sia nella cartella";
+$dataImport = [];
+$readCsv = fopen($csvName, 'r');
+fgetcsv($readCsv);
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    while (($data = fgetcsv($readCsv, 1000, ",")) !== false) { // Legge una riga alla volta
+        print_r($data[0]); // Stampa l'array con i dati della riga
+        print_r($data[1]); // Stampa l'array con i dati della riga
+        $id = $data[0];
+        $name = $data[1];
+        $sql = "INSERT IGNORE INTO categories (id, `name`) VALUES (:id, :name)";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['id' => $id, 'name' => $name]);
+        echo "New record created successfully";
+    }
+
+    fclose($readCsv); // Chiudi il file dopo aver finito
+
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+    fclose($readCsv); // Chiudi il file dopo aver finito
+}
