d21038e5d8d40419dfb7b7a6bf805f4c4a8de735
Mftr96 <matteo.federici96@gmail.com>
2025-02-23 18:51:22 +0100

    milestone 2 part 1(creating db and tables with php script)

--- script.php
+++ script.php
@@ -14,18 +14,50 @@ try {
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "Database created successfully<br>";
-} catch(PDOException $e) {
-  echo   "<br>" . $e->getMessage();
+} catch (PDOException $e) {
+  echo "<br>" . $e->getMessage();
 }
-//vedere perchè diventa null(secondo me per motivi di sicurezza così non rimane traccia )
+//adding variables dbname for table queries
+$dbname = "php_csv_import";
+
+//creating categories and product tables 
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // sql to create table
+  $sql = "CREATE TABLE `categories` (
+    `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NULL
+);";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Table MyGuests created successfully";
+} catch (PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // sql to create table
+  $sql = "CREATE TABLE `products` (
+    `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NULL,
+    `price` DECIMAL(8, 2) NULL,
+    `category_id` SMALLINT UNSIGNED NULL, 
+    CONSTRAINT `products_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`)
+);";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Table MyGuests created successfully";
+} catch (PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
 $conn = null;
-
-
-
-
-
-
-
-
-
 ?>
